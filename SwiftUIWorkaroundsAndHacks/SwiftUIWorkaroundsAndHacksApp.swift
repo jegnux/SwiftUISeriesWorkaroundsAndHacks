623e8a316e3c6e206dd5bca342a43b70e1d456ee
Jérôme Alves <j.alves@me.com>
2022-05-23 21:09:42 +0200

    Xcode setup

---
+++ SwiftUIWorkaroundsAndHacks/SwiftUIWorkaroundsAndHacksApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftUIWorkaroundsAndHacksApp.swift
+//  SwiftUIWorkaroundsAndHacks
+//
+//  Created by Jérôme Alves on 23/05/2022.
+//
+
+import SwiftUI
+
+@main
+struct SwiftUIWorkaroundsAndHacksApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
